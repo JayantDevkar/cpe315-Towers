4d5cd29277294f67e43420ea76b31973dea42034
Jayant Devkar <55962509+JayantDevkar@users.noreply.github.com>
2020-07-06 14:09:48 -0700

    Update towers.s
    final push

--- towers.s
+++ towers.s
@@ -18,26 +18,68 @@ startstring:
 
     .global	towers
 towers:
-   /* Save calllee-saved registers to stack */
-   
+   push {r4, r5, r6, r7, r8, lr}
+/* Save calllee-saved registers to stack */
+   mov r8, r0 //copy of numdisc
+   mov r4, r1 //copy of start
+   mov r5, r2 //copy of goal
+
+   mov r6, #0 //initialize steps
+
    /* Save a copy of all 3 incoming parameters */
 
 if:
    /* Compare numDisks with 2 or (numDisks - 2)*/
    /* Check if less than, else branch to else */
-   
+   cmp r8, #2 //comparing
+   bge else     //branching to else
+   mov r0,r1   //setting up parameters for print
+   mov r1,r2  //setting up parameters for print
+   bl print
+
+   mov r0, #1 //return value 1
+   bl endif  //branching endif
    /* set print function's start to incoming start */
    /* set print function's end to goal */
    /* call print function */
    /* Set return register to 1 */
    /* branch to endif */
+
 else:
    /* Use a callee-saved varable for temp and set it to 6 */
    /* Subract start from temp and store to itself */
    /* Subtract goal from temp and store to itself (temp = 6 - start - goal)*/
 
    /* subtract 1 from original numDisks and store it to numDisks parameter */
+   mov r7, #6 //initialize peg
+   sub r7, r7, r4
+   sub r7, r7, r5
 
+
+
+   sub r0, r8, #1 //temp
+   mov r1,r4
+   mov r2,r7
+   bl towers
+
+   mov  r6, r0 //steps = return vlaue
+
+   mov r0, #1 //setting the next function call
+   mov r1,r4
+   mov r2,r5
+
+   bl towers
+
+   add r6, r6, r0 //adding the return value to steps
+   sub r0, r8, #1
+   mov r1,r7
+   mov r2,r5
+
+   bl towers
+
+   add r6, r6, r0 //adding the results
+
+   mov r0, r6 //loading return
    /* Set end parameter as temp */
    /* Call towers function */
    /* Save result to callee-saved register for total steps */
@@ -46,7 +88,7 @@ else:
    /* Set goal parameter to original goal */
    /* Call towers function */
    /* Add result to total steps so far */
-   
+
    /* Set numDisks parameter to original numDisks - 1 */
    /* set start parameter to temp */
    /* set goal parameter to original goal */
@@ -55,6 +97,8 @@ else:
 
 endif:
    /* Restore Registers */
+   pop {r4, r5, r6, r7, r8, pc}
+
 
 @ Function main is complete, no modifications needed
     .global	main
